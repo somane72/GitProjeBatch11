75ef2af4cf7abfa1a5cfccbe4542578b83c5c717
somane72 <usame1972@gmail.com>
2020-11-16 22:01:34 +0100

    ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,7 +2,9 @@
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("11:57 - Birinci Gun");
+		System.out.println("Ikinci Commit");
 
 	}
 
