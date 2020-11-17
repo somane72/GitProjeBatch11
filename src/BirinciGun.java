58b72bdbcb04b5273faf420d88a7ef89dc2db283
somane72 <67170008+somane72@users.noreply.github.com>
2020-11-17 20:14:25 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,10 +1,10 @@
-
 public class BirinciGun {
 
 	public static void main(String[] args) {
 		
 		System.out.println("11:57 - Birinci Gun");
 		System.out.println("Ikinci Commit");
+		System.out.println("10:11 - Ikinci Gun");
 
 	}
 
