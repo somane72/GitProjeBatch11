cd228a0f0b1185b39cf6101706ace209b9ee0d0f
somane72 <67170008+somane72@users.noreply.github.com>
2020-11-17 21:25:16 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -8,6 +8,7 @@ public class BirinciGun {
 		System.out.println("10:26 - ikinci gun");
 		System.out.println("10:35 - ikinci gun");
 		System.out.println("11:05 - local");
+		System.out.println("11:23 - github.com");
 
 	}
 
