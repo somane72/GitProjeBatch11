27979bac0ef64b1aa6b719e67757b6bfac61c62f
somane72 <usame1972@gmail.com>
2020-11-17 20:29:39 +0100

    ikinci gun ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -5,6 +5,7 @@ public class BirinciGun {
 		System.out.println("11:57 - Birinci Gun");
 		System.out.println("Ikinci Commit");
 		System.out.println("10:11 - Ikinci Gun");
+		System.out.println("10:26 - ikinci gun");
 
 	}
 
