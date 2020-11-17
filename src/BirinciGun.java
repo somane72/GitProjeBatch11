009852e4de7d46ce6bfe51907550c7f779165510
somane72 <usame1972@gmail.com>
2020-11-17 21:06:43 +0100

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,6 +7,7 @@ public class BirinciGun {
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:26 - ikinci gun");
 		System.out.println("10:35 - ikinci gun");
+		System.out.println("11:05 - local");
 
 	}
 
